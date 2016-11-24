bf37e234e9ee3eddd81b127c3c09e0473c6c567a
Dani Avram <dani.avram91@gmail.com>
2016-11-24 17:42:53 +0200

    Initial Commit

---
+++ MobileLevel1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobileLevel1
+//
+//  Created by Daniel Avram on 24/11/2016.
+//  Copyright © 2016 Daniel Avram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
